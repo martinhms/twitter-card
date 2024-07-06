b6a9eba576be971514be4a49f57147fa72b214ee
Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>
2024-07-06 12:40:52 -0300

    [CARD-FIX] - Refactor somme imports and literals

--- app/src/main/java/com/example/twittercard/MainActivity.kt
+++ app/src/main/java/com/example/twittercard/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
